0905fd6e7ac582d77d10b7e34913249529978cf3
TovahParnes <tovah@parnes.com>
2022-10-22 19:59:49 +0200

    added testing dependencies, can't get tests to work

---
+++ HomeExam/scr/TestMain/TestFile.java
@@ -0,0 +1,13 @@
+package HomeExam.scr.TestMain;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TestFile {
+
+    //@test
+    public void test() {
+        assertEquals(1, 1);
+    }
+
+}
